89f31dd4bbbc1e53ed0e3a66ace4ca75ff64ca3c
agomez <agomezdelamo@gmail.com>
2019-08-16 20:27:36 +0200

    dark captain

--- clase2/destructuracion_objetos.js
+++ clase2/destructuracion_objetos.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 /**
 La destructuracion de objetos sirve
 para hacer un model to command basicamente,
@@ -6,9 +6,9 @@ es decir, pasas varias propiedades de un objeto complejo
 a variables simples de una forma practica.
 */
 var avenger = {
-    nombre: "Steve",
-    clave: "Capitan America",
-    poder: "Droga"
+  nombre: 'Steve',
+  clave: 'Capitan America dark',
+  poder: 'Droga'
 };
 var nombre2 = avenger.nombre;
 var clave2 = avenger.clave;
@@ -20,11 +20,15 @@ var poder2 = avenger.poder;
 // da igual el orden de las propiedades al desesctructurarlo,
 // lo importante es que existan en el objeto.
 //GENERO 3 VARIABLES QUE SON PROPIEDADES DEL OBJETO AVENGER
-var clave = avenger.clave, poder = avenger.poder, nombre = avenger.nombre;
+var clave = avenger.clave,
+  poder = avenger.poder,
+  nombre = avenger.nombre;
 console.log(nombre, clave, poder);
 //tambien sirve en arrays, solo que se desestructura por posicion.
-var avengersArray = ["Thor", "Steve", "Tony"];
+var avengersArray = ['Thor', 'Steve', 'Tony'];
 // en este caso no tira del nombre de la propiedad
 //tira de la posicion del array, por eso loki tiene de valor thor.
-var loki = avengersArray[0], capi = avengersArray[1], ironMan = avengersArray[2];
+var loki = avengersArray[0],
+  capi = avengersArray[1],
+  ironMan = avengersArray[2];
 console.log(avengersArray);
